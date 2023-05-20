5d8d90423d68b249a2cdc81c67c3180a54a2afd2
MaxTkachuk03 <max200325622@gmail.com>
2023-05-20 13:42:52 +0300

    first commit

---
+++ lib/pages/enter_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EntrancePage extends StatefulWidget {
+  const EntrancePage({super.key});
+
+  @override
+  State<EntrancePage> createState() => _EntrancePageState();
+}
+
+class _EntrancePageState extends State<EntrancePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
